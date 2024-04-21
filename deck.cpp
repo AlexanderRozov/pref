e79e69570259c82508609652a3609e964861b65d
alexander.rozov <alexander.rozov@generalarcade.com>
2024-04-21 22:45:45 +0400

    init

---
+++ deck.cpp
@@ -0,0 +1,5 @@
+//
+// Created by fog54 on 21.04.2024.
+//
+
+#include "deck.h"
---
+++ deck.h
@@ -0,0 +1,14 @@
+//
+// Created by fog54 on 21.04.2024.
+//
+
+#ifndef PREF_DECK_H
+#define PREF_DECK_H
+
+
+class deck {
+
+};
+
+
+#endif //PREF_DECK_H
