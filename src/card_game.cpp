fc80b77afba02ab6c0f94bf081902f8774ddc856
alexander.rozov <alexander.rozov@generalarcade.com>
2024-04-22 00:04:33 +0400

    first classes

---
+++ src/card_game.cpp
@@ -0,0 +1,5 @@
+//
+// Created by fog54 on 21.04.2024.
+//
+
+#include "card_game.h"
---
+++ src/card_game.h
@@ -0,0 +1,14 @@
+//
+// Created by fog54 on 21.04.2024.
+//
+
+#ifndef PREF_CARD_GAME_H
+#define PREF_CARD_GAME_H
+
+
+class card_game {
+
+};
+
+
+#endif //PREF_CARD_GAME_H
