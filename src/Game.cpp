29cafdddb173bf32ff75cbeba70b418c7e73d677
alexander.rozov <alexander.rozov>
2024-04-27 01:52:59 +0400

    Abstract Game classes

---
+++ src/Game.cpp
@@ -0,0 +1,20 @@
+//
+// Created by fog54 on 27.04.2024.
+//
+
+#include "Game.h"
+
+void Game::ChangeState()
+{
+
+}
+
+void Game::StartGame()
+{
+
+}
+
+void Game::EndGame()
+{
+
+}
---
+++ src/Game.h
@@ -0,0 +1,21 @@
+//
+// Created by fog54 on 27.04.2024.
+//
+
+#ifndef PREF_GAME_H
+#define PREF_GAME_H
+#include "Gamer.h"
+
+class Game
+{
+
+    virtual void ChangeState();
+    virtual void StartGame();
+    virtual void EndGame();
+
+
+
+};
+
+
+#endif //PREF_GAME_H
