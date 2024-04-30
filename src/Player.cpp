05f460ae83e2ac7d91b0b0bff60c4413ec0b303b
alexander.rozov <alexander.rozov>
2024-05-01 01:54:04 +0400

    test

---
+++ src/Player.cpp
@@ -0,0 +1,23 @@
+//
+// Created by fog54 on 23.04.2024.
+//
+
+#include <algorithm>
+#include "Player.h"
+
+Card Player::SetCardOnTable(Card card)
+{
+    Card c;
+    return  c;
+
+}
+
+void Player::GetCardFromDeck(Card card)
+{
+    Hand.push_back(card);
+}
+
+void Player::GetCardFromDeck()
+{
+
+}
